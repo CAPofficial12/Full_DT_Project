2043b14ef701687cd17b7e7749640c71b5b95d9a
Aarav <athakur@dohacollege.com.qa>
2026-05-22 07:34:30 +0300

    Got LED blinking using raw AVR code

---
+++ Robotics code/Practice.Build Up code/Arduino Uno/Blink_LED/src/main.cpp
@@ -0,0 +1,15 @@
+#define F_CPU  20000000
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+int main(void){ 
+  DDRB = 0X01;
+  PORTB = 0;
+  while (1){
+    PORTB = 0x01;
+    _delay_ms(1000);
+    PORTB = 0;
+    _delay_ms(1000);
+  }
+}
