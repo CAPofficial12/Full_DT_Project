674c9bce876c90a1ad73c55116726ff03724d93f
Aarav <athakur@dohacollege.com.qa>
2026-05-23 18:41:35 +0300

    Moved PIDF to Arduin Uno Code

--- Robotics code/Practice.Build Up code/Arduino Uno/PWM Signal/src/main.cpp
+++ Robotics code/Practice.Build Up code/Arduino Uno/PWM Signal/src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/delay.h>
@@ -24,4 +25,28 @@ int main(){
 }
 ISR(TIMER0_OVF_vect){
   OCR0A = (dutyCycle/100*255);
+}
+*/
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(25);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 5) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(25);                       // waits 15ms for the servo to reach the position
+  }
 }
