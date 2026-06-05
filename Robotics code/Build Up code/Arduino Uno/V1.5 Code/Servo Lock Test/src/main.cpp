99e09f87c4eed9140dd4f16f8da28791572880b9
Aarav <athakur@dohacollege.com.qa>
2026-06-05 17:22:01 +0300

    Servo Test 8: Servo and bracket boundary calibration

--- Robotics code/Build Up code/Arduino Uno/V1.5 Code/Servo Lock Test/src/main.cpp
+++ Robotics code/Build Up code/Arduino Uno/V1.5 Code/Servo Lock Test/src/main.cpp
@@ -6,24 +6,20 @@ Servo TopRight;
 Servo BottomLeft;
 Servo BottomRight;
 
-int brightness = 0;
-int multiplier = 1;
 void Lock_Servo(int);                                                                                       
 
 void setup() {
+  TopLeft.attach(5);
   TopRight.attach(6);
-  BottomLeft.attach(9);
-  BottomRight.attach(10);
-  pinMode(5, OUTPUT);
 }
 
 void loop() {
-
-  analogWrite(5, 100);
-  delay(1000);
+  TopRight.write(0);
+  TopLeft.write(180);
 }
 
 void Lock_Servo(int angle){
-  long long milli = map(angle, 0, 180, 0, 255);
-  TopLeft.write(angle);
+  long milli = map(angle, 0, 180, 0, 255);
+  analogWrite(5, milli);
+  analogWrite(6, milli);
 }
