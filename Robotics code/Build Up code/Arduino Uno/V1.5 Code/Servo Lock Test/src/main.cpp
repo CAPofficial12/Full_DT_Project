808b7d07a24f4a718a2120e99521013f6cd43d60
Aarav <athakur@dohacollege.com.qa>
2026-06-05 18:36:17 +0300

    Servo Test 9: Platform 90

--- Robotics code/Build Up code/Arduino Uno/V1.5 Code/Servo Lock Test/src/main.cpp
+++ Robotics code/Build Up code/Arduino Uno/V1.5 Code/Servo Lock Test/src/main.cpp
@@ -6,20 +6,20 @@ Servo TopRight;
 Servo BottomLeft;
 Servo BottomRight;
 
-void Lock_Servo(int);                                                                                       
+void PlatforM(int);                                                                            
 
 void setup() {
   TopLeft.attach(5);
   TopRight.attach(6);
+  BottomLeft.attach(9);
+  BottomRight.attach(10);
 }
 
 void loop() {
-  TopRight.write(0);
-  TopLeft.write(180);
+  PlatforM(90);
 }
 
-void Lock_Servo(int angle){
-  long milli = map(angle, 0, 180, 0, 255);
-  analogWrite(5, milli);
-  analogWrite(6, milli);
+void PlatforM(int angle){
+  TopRight.write(angle);
+  TopLeft.write(180-angle);
 }
