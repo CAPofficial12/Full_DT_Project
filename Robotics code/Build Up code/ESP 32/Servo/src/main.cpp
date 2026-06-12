d2d422179b26d6663b3bc18c1ddd02dda86ebcc0
Aarav <athakur@dohacollege.com.qa>
2026-06-12 20:42:24 +0300

    Servo Test 13: Servo Lock on ESP32

--- Robotics code/Build Up code/ESP 32/Servo/src/main.cpp
+++ Robotics code/Build Up code/ESP 32/Servo/src/main.cpp
@@ -1,22 +1,24 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-const int ServoPin = 23;
-Servo testServo;
+const int BottomLeftPin = 23;
+const int BottomRightPin = 22;
+
+Servo BottomLeft;
+Servo BottomRight;
 
 void setup() {
   ESP32PWM::allocateTimer(0);
-  testServo.setPeriodHertz(50); //Test at 333Hz
-  testServo.attach(ServoPin, 500, 2500);
+  ESP32PWM::allocateTimer(1);
+
+  BottomLeft.setPeriodHertz(50); //Test at 333Hz
+  BottomRight.setPeriodHertz(50);
+
+  BottomLeft.attach(BottomLeftPin, 500, 2500);
+  BottomRight.attach(BottomRightPin, 500, 2500); 
 }
 
 void loop() {
-  testServo.write(0);
-  delay(1000);
-  testServo.write(90);
-  delay(1000);
-  testServo.write(180);
-  delay(1000);
-  testServo.write(90);
-  delay(1000);
+  BottomLeft.write(90);
+  BottomRight.write(90);
 }
