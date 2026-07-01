43a60dc16154bab283c97e66372867c4da6c640b
Aarav <athakur@dohacollege.com.qa>
2026-07-01 13:39:09 +0300

    Comppleted WiFi NTP test

--- Robotics code/Build Up code/ESP 32/WiFi Tests/Simple WiFi Test/src/main.cpp
+++ Robotics code/Build Up code/ESP 32/WiFi Tests/Simple WiFi Test/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Internet details
+const char* SSID = "TP-Link_1650";
+const char* password = "57794884";
+
+//NTP server
+const char* server = "pool.ntp.org";
+
+// TimeZone Offset
+const long GMT_offset = 0;
+const long DST_offset = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  WiFi.begin(SSID, password);
+  Serial.begin(115200);
+  Serial.print("Connecting to Wifi...");
+  while (WiFi.status() != WL_CONNECTED){  //Checks if ESP32 has connected to Wifi
+    delay(300);
+    Serial.print(".");
+  }
+  Serial.print("Connected to WiFi");
+  configTime(GMT_offset, DST_offset, server);
+  Serial.print("Time Initlaised");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain Time");
+    delay(1000);
+    return;
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.printf("Date: %04d-%02d-%02d ",
+                timeinfo.tm_year + 1900,
+                timeinfo.tm_mon + 1,
+                timeinfo.tm_mday);
+  Serial.printf("Date: %04d-%02d-%02d \n",
+                timeinfo.tm_hour,
+                timeinfo.tm_min,
+                timeinfo.tm_sec);
+  delay(1000);
 }
