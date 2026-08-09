13b877e43cd10a546a3b537e5c6f12ebb83e46d0
Aarav <athakur@dohacollege.com.qa>
2026-08-09 19:13:31 +0530

    Rewrote Motor Test

--- Robotics code/Build Up code/ESP 32/Movement Test/TT Motor Test/src/main.cpp
+++ Robotics code/Build Up code/ESP 32/Movement Test/TT Motor Test/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 
-int enA = 1;
-int enB = 2;
-int in1 = 3;
-int in2 = 4;
-int in3 = 5;
-int in4 = 6;
+const int enA = 1;
+const int enB = 2;
+const int in1 = 3;
+const int in2 = 4;
+const int in3 = 5;
+const int in4 = 6;
 
 void setup(){
 
@@ -24,20 +24,26 @@ void setup(){
 
 void loop() {
   analogWrite(enA, 255);
+  analogWrite(enB, 255);
   delay(3000);
 
   analogWrite(enA, 200);
+  analogWrite(enB, 200);
   delay(3000);
 
   analogWrite(enA, 150);
+  analogWrite(enA, 255);
   delay(3000);
 
   analogWrite(enA, 100);
+  analogWrite(enA, 255);
   delay(3000);
 
   analogWrite(enA, 50);
+  analogWrite(enA, 255);
   delay(3000);
 
+  analogWrite(enA, 0);
   analogWrite(enA, 0);
   delay(3000);
 }
